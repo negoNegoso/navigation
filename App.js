0b75d325ec74b177ba5d43b933d1929db488d915
Yuri <yurimoreiravillanova1@gmail.com>
2024-11-05 16:33:47 -0300

    feat: create component loading

--- App.js
+++ App.js
@@ -2,11 +2,15 @@
 import { Groups } from '@screens/groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme'
+import { ActivityIndicator } from 'react-native';
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const loading = true 
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!loading ? <Groups /> : <Loading/>}
     </ThemeProvider>
 
   );
